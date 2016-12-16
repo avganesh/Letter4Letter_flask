5584ad6634730449f286502db2aee385438699e3
avganesh <avganeshalingam@gmail.com>
2016-12-16 15:21:01 -0500

    Updated homepage and play offline

--- app.py
+++ app.py
@@ -8,16 +8,19 @@ heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 # Create our database model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+class l4l_games(db.Model):
+    __tablename__ = "l4l_games"
+    #id = db.Column(db.Integer, primary_key=True)
+    gameid = db.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = db.Column(db.String(120), unique=True)
+    wordgamestate = db.Column(db.String(120), unique=False)
+    score = db.Column(db.Integer, unique=False)
 
-    def __init__(self, email):
-        self.email = email
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
-        return '<E-mail %r>' % self.email
+        return '<Name %r>' % self.name
 
 # Set "homepage" to index.html and Save e-mail to database 
 @app.route('/')
@@ -27,16 +30,60 @@ def index():
 # Save e-mail to database and send to success page
 @app.route('/', methods=['POST'])
 def prereg():
-    email = None
+    name = None
     if request.method == 'POST':
-        email = request.form['email']
+        name = request.form['name']
         # Check that email does not already exist (not a great query, but works)
-        if not db.session.query(User).filter(User.email == email).count():
-            reg = User(email)
+        if not db.session.query(l4l_games).filter(l4l_games.name == name).count():
+            reg = l4l_games(name)
             db.session.add(reg)
             db.session.commit()
             return render_template('index.html')
     return render_template('index.html')
+
+@app.route('/playoffline', methods=['GET'])
+def playoffline():
+    wordgamestate = None
+    if request.method == 'GET':
+        wordgamestate = ""
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(l4l_games).filter(l4l_games.wordgamestate == wordgamestate and l4l_games.gameid == gameid).count():
+            reg = l4l_games(wordgamestate)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('playoffline.html')
+    return render_template('playoffline.html')
+
+@app.route('/playonline', methods=['GET'])
+def playonline():
+    wordgamestate = None
+    if request.method == 'GET':
+        wordgamestate = ""
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(l4l_games).filter(l4l_games.wordgamestate == wordgamestate and l4l_games.gameid == gameid).count():
+            reg = l4l_games(wordgamestate)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('playonline.html')
+    return render_template('playonline.html')
+
+@app.route('/playmove', methods=['POST'])
+def playmove():
+    wordgamestate = None
+    if request.method == 'POST':
+        wordgamestate = request.form['theletter']
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(l4l_games).filter(l4l_games.wordgamestate == wordgamestate and l4l_games.gameid == gameid).count():
+            reg = l4l_games(wordgamestate)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('playonline.html')
+        else:
+            reg = l4l_games(wordgamestate)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('playonline.html')
+    return render_template('playonline.html')
 
 if __name__ == '__main__':
     app.debug = True
