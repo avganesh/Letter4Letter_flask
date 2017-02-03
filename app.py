9ddf9e6d987f3e644a6f09ba3e8323242caaf074
avganesh <avganeshalingam@gmail.com>
2017-02-03 14:02:27 -0500

    Update AJAX post to json

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class l4l_games(db.Model):
         return '<Name %r>' % self.P1name
 
 # Set "homepage" to index.html
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -36,21 +36,21 @@ def index():
 ##    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 # Save e-mail to database and send to success page
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/newgame', methods=['GET', 'POST'])
 def newgame():
     if request.method == 'POST':
-        P1name = request.form['name']
+        P1name = request.json['name']
         reg = l4l_games(P1name)
         db.session.add(reg)
         db.session.commit()
         game = l4l_games.query.filter_by(P1name=P1name).first()
         return render_template('playonline.html', Player1=game.P1name, Player2=game.P2name, gameid=game.gameid,  theword=game.wordgamestate, P1score=game.P1score, P2score=game.P2score)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/joingame', methods=['GET', 'POST'])
 def joingame():
     if request.method == 'POST':
-        P2name = request.form['name']
-        gameid = request.form['gameid']
+        P2name = request.json['name']
+        gameid = request.json['gameid']
         game = db.session.query(l4l_games).filter(gameid==gameid).first()
         game.P2name = P2name 
         return render_template('playonline.html', Player1=game.P1name, Player2=game.P2name, gameid=game.gameid,  theword=game.wordgamestate, P1score=game.P1score, P2score=game.P2score)
