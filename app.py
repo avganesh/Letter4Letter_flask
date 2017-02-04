79633307336a7643f0ec9e84016005a9b9b71d14
avganesh <avganeshalingam@gmail.com>
2017-02-03 21:06:14 -0500

    added name back

--- app.py
+++ app.py
@@ -111,6 +111,7 @@ def keepscore():
 def refresh():
     if request.method == 'POST':
         gameid = request.json['gameData']
+        name = request.json['playerData']
         game = l4l_games.query.filter_by(gameid=gameid).first()
     return render_template('playonline.html', Player1=game.P1name, Player2=game.P2name, gameid=game.gameid,  theword=game.wordgamestate, P1score=game.P1score, P2score=game.P2score, lastmove=game.lastmove, roundnum=len(game.wordgamestate), yourname=name)
 
